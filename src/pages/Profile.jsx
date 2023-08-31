56f2b4fe1f7580b46b2297d0c491ce669f27a845
Ángel Arias <angadominguez96@gmail.com>
2023-08-31 18:47:26 +0200

    user email edit feature

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,19 +1,31 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import service from "../services/service.config";
-import {Popover, PopoverTrigger, PopoverContent, Button, Input} from "@nextui-org/react";
-
-
-
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  Button,
+  Input,
+  Card,
+  CardBody,
+} from "@nextui-org/react";
 
 export default function Profile() {
   const navigate = useNavigate();
   const [userProfile, setUserProfile] = useState();
-
+  const [email, setEmail] = useState("");
+  const [errorMessage, setErrorMessage] = useState("")
 
   useEffect(() => {
     getData();
   }, []);
+
+  useEffect(() => {
+    if (userProfile && userProfile.email) {
+        setEmail(userProfile.email);
+    }
+  }, [userProfile]);
 
   const getData = async () => {
     try {
@@ -26,46 +38,64 @@ export default function Profile() {
     }
   };
 
- const handleEmailForm = async (e) => {
-    e.preventDefault()
+  const handleEmailForm = async (e) => {
+    e.preventDefault();
     try {
-        await service.patch("/account/profile/edit-email")
-
+      await service.patch("/account/profile/edit-email", {
+        email
+      });
+      setUserProfile(prevUserProfile => ({ ...prevUserProfile, email }));
+      navigate("/account/profile");
     } catch (error) {
-        
+      console.log(error);
+      if(error.response && error.response.status === 400){
+        setErrorMessage(error.response.data.errorMessage)
+      } else {
+        console.log(error)
+        navigate("/error")
+      }
     }
+  };
+
+  if (userProfile === undefined) {
+    return <h3>BUSCANDO</h3>;
   }
 
-if (userProfile === undefined){
-    return <h3>BUSCANDO</h3>
-}
-
-
-
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center h-screen"> 
+    <Card>
+      <CardBody>
       <h1>Perfil de {userProfile.username} </h1>
       <h3>Email: {userProfile.email}</h3>
 
       <Popover placement="bottom" showArrow offset={10}>
-      <PopoverTrigger>
-        <Button color="primary">Change your Email</Button>
-      </PopoverTrigger>
-      <PopoverContent className="w-[240px]">
-        {() => (
-          <div className="px-1 py-2 w-full">
-            
-            <div  className="mt-2 flex flex-col gap-2 w-full">
-              <Input onSubmit={handleEmailForm} defaultValue={userProfile.email} label="email" size="sm" variant="bordered" name="email" />
-            
+        <PopoverTrigger>
+          <Button color="primary">Change your Email</Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-[240px]">
+          {() => (
+            <div className="px-1 py-2 w-full">
+              <form className="mt-2 flex flex-col gap-2 w-full">
+                <Input
+                  defaultValue={userProfile.email}
+                  label="email"
+                  size="sm"
+                  variant="bordered"
+                  name="email"
+                  value={email}
+                  onChange={(e) => { setEmail(e.target.value) }}
+                />
+                <Button className="hidden" type="submit" onClick={handleEmailForm}/>
+                {errorMessage ? <p> {errorMessage}</p> : null}
+              </form>
             </div>
-          </div>
-        )}
-      </PopoverContent>
-    </Popover>
+          )}
+        </PopoverContent>
+      </Popover>
 
       <img src={userProfile.profilePic} alt="Profile Pic" />
-
+      </CardBody>
+    </Card>
     </div>
   );
 }
